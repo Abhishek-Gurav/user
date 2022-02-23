62db0c41298a723f88c8a79961935f785547ad88
Sarfraz-droid <Sarf.alam0206>
2022-02-23 20:33:08 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import Homepage from "./pages/Homepage";
 import Edit from "./pages/Edit";
 import Add from "./pages/Add";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router , 
+import {
+  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import React from "react";
 function App() {
   return (
-    // <Homepage />
-    <Router>
-    <div>
-      <Switch>
-      <Route path="/" > <Homepage /> </Route>
-      <Route path="/edit"> <Edit /> </Route>
-      <Route path="/add"> <Add /> </Route>
-      </Switch>
-      </div>
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/edit" component={Edit} />
+          <Route exact path="/add" component={Add} />
+
+        </Switch>
+      </Router>
+    </React.Fragment>
   )
 }
 
